f993cd67dfefc7265cef212288f332ed62672022
Radnoumane Mossabely <radnou.m@gmail.com>
2023-12-02 03:02:18 +0100

    Initial commit

---
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+    @Test
+    void myFirstTest(){
+        assertThat(42).isEqualTo(42);
+    }
+}
