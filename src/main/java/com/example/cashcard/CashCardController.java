42cbf17a22cc2b519df4b1d978b6082ebabcbe04
Radnoumane Mossabely <radnou.m@gmail.com>
2023-12-02 21:53:24 +0100

    feat: Add createCashCard and findAll methods

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,29 +1,56 @@
 package com.example.cashcard;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
-private final CashCardRepository cashCardRepository;
+    private final CashCardRepository cashCardRepository;
 
-     private CashCardController(CashCardRepository cashCardRepository) {
+    private CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-        if (cashCardOptional.isPresent()){
+        if (cashCardOptional.isPresent()) {
             return ResponseEntity.ok(cashCardOptional.get());
-        }else {
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping
+    private ResponseEntity createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
+    }
+
 }
--- src/main/java/com/example/cashcard/CashCardRepository.java
+++ src/main/java/com/example/cashcard/CashCardRepository.java
@@ -1,5 +1,8 @@
 package com.example.cashcard;
-import org.springframework.data.repository.CrudRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard,Long>{
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long>,
+        PagingAndSortingRepository<CashCard, Long> {
 }
